fa12f53588b22e0cf68046c0a1f1d67011223c5e
Shaveen12 <126313485+Shaveen12@users.noreply.github.com>
2024-04-21 09:17:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_httpauth import HTTPBasicAuth
+from flask_cors import CORS
 import pandas as pd
 
 
 app = Flask(__name__)
+CORS(app)
 auth = HTTPBasicAuth()
 
 users = {
